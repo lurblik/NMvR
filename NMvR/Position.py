96159e15b05d832f1ea7fcf9dc5b06810b3e73e6
lurblik <91660199+lurblik@users.noreply.github.com>
2021-11-30 01:28:16 -0800

    added position file

---
+++ NMvR/Position.py
@@ -0,0 +1,9 @@
+class Position():
+    def __init__(self, x, y, theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
+class Speed():
+    def __init__(self, lin_vel, ang_vel):
+        self.lin_vel = lin_vel
+        self.ang_vel = ang_vel
